9f850a3c93cd003736d033714c8d2a3516400be0
Gabriel Plassard <gabriel.plassard@gmail.com>
2022-12-08 00:28:34 +0100

    chore(mvp): minimal stack policy construct

--- .projenrc.ts
+++ .projenrc.ts
@@ -5,5 +5,7 @@ import { TypescriptLibraryProject } from '@gplassard/projen-extensions';
 const project = new TypescriptLibraryProject({
     name: 'cdk-cloudformation-extensions', 
     packageName: '@gplassard/cdk-cloudformation-extensions',
+    devDeps: ['aws-cdk-lib', 'constructs'],
+    peerDeps: ['aws-cdk-lib', 'constructs'],
 });
 project.synth();
