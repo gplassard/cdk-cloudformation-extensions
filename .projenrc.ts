392657e60a2614198bb8c782814c6cee54eea1ee
Gabriel Plassard <gabriel.plassard@gmail.com>
2022-12-08 00:34:09 +0100

    chore(mvp): minimal stack policy construct (#1)
    * chore(mvp): minimal stack policy construct
    
    * chore: self mutation
    
    Signed-off-by: github-actions <github-actions@github.com>
    
    Signed-off-by: github-actions <github-actions@github.com>
    Co-authored-by: github-actions <github-actions@github.com>

--- .projenrc.ts
+++ .projenrc.ts
@@ -3,7 +3,9 @@ import { TypescriptLibraryProject } from '@gplassard/projen-extensions';
 
 // opinionated wrapper around projen TypeScriptProject for libraries
 const project = new TypescriptLibraryProject({
-    name: 'cdk-cloudformation-extensions', 
-    packageName: '@gplassard/cdk-cloudformation-extensions',
+  name: 'cdk-cloudformation-extensions',
+  packageName: '@gplassard/cdk-cloudformation-extensions',
+  devDeps: ['aws-cdk-lib', 'constructs'],
+  peerDeps: ['aws-cdk-lib', 'constructs'],
 });
 project.synth();
---
+++ src/stack-policy/StackPolicy.ts
@@ -0,0 +1,55 @@
+import { Duration, Stack } from 'aws-cdk-lib';
+import { RetentionDays } from 'aws-cdk-lib/aws-logs';
+import { AwsCustomResource, AwsCustomResourcePolicy, PhysicalResourceId } from 'aws-cdk-lib/custom-resources';
+import { Construct } from 'constructs';
+
+export interface StackPolicyProps {
+  readonly protectedStack: Stack;
+  readonly policy: string;
+}
+
+export class StackPolicy extends Construct {
+  constructor(scope: Construct, id: string, props: StackPolicyProps) {
+    super(scope, id);
+
+    new AwsCustomResource(this, `custom-resource-${props.protectedStack.stackName}`, {
+      timeout: Duration.minutes(2),
+      logRetention: RetentionDays.ONE_WEEK,
+      onCreate: {
+        service: 'CloudFormation',
+        action: 'setStackPolicy',
+        parameters: {
+          StackName: props.protectedStack.stackName,
+          StackPolicyBody: props.policy,
+        },
+        physicalResourceId: PhysicalResourceId.of(`policy-${props.protectedStack.stackName}`),
+      },
+      onUpdate: {
+        service: 'CloudFormation',
+        action: 'setStackPolicy',
+        parameters: {
+          StackName: props.protectedStack.stackName,
+          StackPolicyBody: props.policy,
+        },
+        physicalResourceId: PhysicalResourceId.of(`policy-${props.protectedStack.stackName}`),
+      },
+      onDelete: {
+        service: 'CloudFormation',
+        action: 'setStackPolicy',
+        parameters: {
+          StackName: props.protectedStack.stackName,
+          StackPolicyBody: JSON.stringify({
+            Statement: {
+              Effect: 'Allow',
+              Action: 'Update:*',
+              Principal: '*',
+              Resource: '*',
+            },
+          }),
+        },
+        physicalResourceId: PhysicalResourceId.of(`policy-${props.protectedStack.stackName}`),
+      },
+      policy: AwsCustomResourcePolicy.fromSdkCalls({ resources: [props.protectedStack.stackId] }),
+    });
+  }
+}
