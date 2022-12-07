3b727c8d53940c997e05c66fd255f5e593509fb3
Gabriel Plassard <gabriel.plassard@gmail.com>
2022-12-08 00:12:35 +0100

    chore(setup): projen init

---
+++ .projenrc.ts
@@ -0,0 +1,9 @@
+// .projenrc.ts
+import { TypescriptLibraryProject } from '@gplassard/projen-extensions';
+
+// opinionated wrapper around projen TypeScriptProject for libraries
+const project = new TypescriptLibraryProject({
+    name: 'cdk-cloudformation-extensions', 
+    packageName: '@gplassard/cdk-cloudformation-extensions',
+});
+project.synth();
